bb6c851c03b49ed4fec046bd283e50783e3b4a25
Matthias Neuner <Matthias.Neuner@uibk.ac.at>
2023-12-11 18:50:22 +0100

    aded penalty equal value constraint

---
+++ examples/013_penalty_equal_value_constraint/penalty_equal_value_test.py
@@ -0,0 +1,267 @@
+# hyperelasticity -*- coding: utf-8 -*-
+#  ---------------------------------------------------------------------
+#
+#  _____    _      _              _         __  __ ____  __  __
+# | ____|__| | ___| |_      _____(_)___ ___|  \/  |  _ \|  \/  |
+# |  _| / _` |/ _ \ \ \ /\ / / _ \ / __/ __| |\/| | |_) | |\/| |
+# | |__| (_| |  __/ |\ V  V /  __/ \__ \__ \ |  | |  __/| |  | |
+# |_____\__,_|\___|_| \_/\_/ \___|_|___/___/_|  |_|_|   |_|  |_|
+#
+#
+#  Unit of Strength of Materials and Structural Analysis
+#  University of Innsbruck,
+#  2023 - today
+#
+#  Matthias Neuner matthias.neuner@uibk.ac.at
+#
+#  This file is part of EdelweissMPM.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  The full text of the license can be found in the file LICENSE.md at
+#  the top level directory of EdelweissMPM.
+#  ---------------------------------------------------------------------
+import pytest
+import argparse
+import numpy as np
+
+from fe.journal.journal import Journal
+from mpm.fields.nodefield import MPMNodeField
+from mpm.fieldoutput.fieldoutput import MPMFieldOutputController
+
+from mpm.generators import rectangulargridgenerator, rectangularmpgenerator
+from mpm.mpmmanagers.simplempmmanager import SimpleMaterialPointManager
+from mpm.mpmmanagers.smartmpmmanager import SmartMaterialPointManager
+from mpm.models.mpmmodel import MPMModel
+from mpm.numerics.dofmanager import MPMDofManager
+from mpm.outputmanagers.ensight import OutputManager as EnsightOutputManager
+from mpm.sets.cellset import CellSet
+from mpm.constraints.penaltyequalvalue import PenaltyEqualValue
+from fe.sets.nodeset import NodeSet
+
+from fe.timesteppers.adaptivetimestepper import AdaptiveTimeStepper
+from mpm.solvers.nqs import NonlinearQuasistaticSolver
+from fe.linsolve.pardiso.pardiso import pardisoSolve
+from mpm.stepactions.dirichlet import Dirichlet
+from mpm.stepactions.bodyload import BodyLoad
+from fe.utils.exceptions import StepFailed
+import fe.utils.performancetiming as performancetiming
+
+import numpy as np
+from datetime import datetime
+
+
+@performancetiming.timeit("simulation")
+def run_sim():
+    dimension = 2
+
+    journal = Journal()
+
+    np.set_printoptions(precision=3)
+
+    mpmModel = MPMModel(dimension)
+
+    rectangulargridgenerator.generateModelData(
+        mpmModel,
+        journal,
+        x0=0.0,
+        l=100.0,
+        y0=0.0,
+        h=100.0,
+        nX=12,
+        nY=12,
+        cellProvider="LagrangianMarmotCell",
+        cellType="GradientEnhancedMicropolar/Quad4",
+    )
+    rectangularmpgenerator.generateModelData(
+        mpmModel,
+        journal,
+        x0=0.01,
+        l=79.98,
+        y0=60.0,
+        h=20.0,
+        nX=60,
+        nY=15,
+        mpProvider="marmot",
+        mpType="GradientEnhancedMicropolar/PlaneStrain",
+    )
+
+    material = "GMDAMAGEDSHEARNEOHOOKE"
+    materialProperties = np.array([30000.0, 0.3, 1.0, 2, 4, 1.4999])
+    for mp in mpmModel.materialPoints.values():
+        mp.assignMaterial(material, materialProperties)
+
+    mpmModel.prepareYourself(journal)
+    mpmModel.nodeFields["displacement"].createFieldValueEntry("dU")
+
+    allCells = mpmModel.cellSets["all"]
+    allMPs = mpmModel.materialPointSets["all"]
+
+    mpmManager = SmartMaterialPointManager(allCells, allMPs, options={"KDTreeLevels": 4})
+
+    journal.printSeperationLine()
+
+    fieldOutputController = MPMFieldOutputController(mpmModel, journal)
+
+    nodeFieldOnAllCells = mpmModel.nodeFields["displacement"].subset(allCells)
+
+    fieldOutputController.addPerNodeFieldOutput("dU", nodeFieldOnAllCells, "dU")
+    fieldOutputController.addPerMaterialPointFieldOutput(
+        "displacement",
+        allMPs,
+        "displacement",
+    )
+
+    fieldOutputController.addPerMaterialPointFieldOutput(
+        "deformation gradient",
+        allMPs,
+        "deformation gradient",
+    )
+
+    fieldOutputController.initializeJob()
+
+    ensightOutput = EnsightOutputManager("ensight", mpmModel, fieldOutputController, journal, None)
+
+    ensightOutput.updateDefinition(fieldOutput=fieldOutputController.fieldOutputs["dU"], create="perNode")
+    ensightOutput.updateDefinition(fieldOutput=fieldOutputController.fieldOutputs["displacement"], create="perNode")
+    ensightOutput.updateDefinition(
+        fieldOutput=fieldOutputController.fieldOutputs["deformation gradient"], create="perNode"
+    )
+
+    ensightOutput.initializeJob()
+
+    outputManagers = [
+        ensightOutput,
+    ]
+
+    dirichletBottom = Dirichlet(
+        "bottom",
+        mpmModel.nodeSets["rectangular_grid_bottom"],
+        "displacement",
+        {1: 0.0},
+        mpmModel,
+        journal,
+    )
+
+    dirichletLeft = Dirichlet(
+        "left",
+        mpmModel.nodeSets["rectangular_grid_left"],
+        "displacement",
+        {0: 0.0, 1: 0.0},
+        mpmModel,
+        journal,
+    )
+
+    dirichletRight = Dirichlet(
+        "right",
+        mpmModel.nodeSets["rectangular_grid_right"],
+        "displacement",
+        {0: 0.0, 1: 0.0},
+        mpmModel,
+        journal,
+    )
+
+    gravityLoad = BodyLoad(
+        "theGravity",
+        mpmModel,
+        journal,
+        mpmModel.cells.values(),
+        "BodyForce",
+        np.array([0.0, -50.0]),
+    )
+
+    constraints = [
+        PenaltyEqualValue(
+            "PenaltyEqualValue", mpmModel, mpmModel.materialPointSets["planeRect_right"], "displacement", 0, 1e6
+        ),
+    ]
+
+    adaptiveTimeStepper = AdaptiveTimeStepper(0.0, 1.0, 1e-2, 1e-2, 1e-3, 1000, journal)
+
+    nonlinearSolver = NonlinearQuasistaticSolver(journal)
+
+    iterationOptions = dict()
+
+    iterationOptions["max. iterations"] = 15
+    iterationOptions["critical iterations"] = 3
+    iterationOptions["allowed residual growths"] = 3
+
+    linearSolver = pardisoSolve
+
+    try:
+        nonlinearSolver.solveStep(
+            adaptiveTimeStepper,
+            linearSolver,
+            mpmManager,
+            [
+                dirichletBottom,
+                dirichletLeft,
+            ],
+            [gravityLoad],
+            constraints,
+            mpmModel,
+            fieldOutputController,
+            outputManagers,
+            iterationOptions,
+        )
+
+    except StepFailed as e:
+        # print(e)
+        raise
+    finally:
+        table = []
+        k1 = "solve step"
+        v1 = performancetiming.times[k1]
+        table.append(("{:}".format(k1), "{:10.4f}s".format(v1.time)))
+        for k2, v2 in v1.items():
+            table.append(("  {:}".format(k2), "  {:10.4f}s".format(v2.time)))
+            for k3, v3 in v2.items():
+                table.append(("    {:}".format(k3), "    {:10.4f}s".format(v3.time)))
+
+        journal.printTable(
+            table,
+            "step 1",
+        )
+
+    fieldOutputController.finalizeJob()
+    ensightOutput.finalizeJob()
+
+    np.savetxt("U.csv", fieldOutputController.fieldOutputs["displacement"].getLastResult())
+
+    return mpmModel
+
+
+@pytest.fixture(autouse=True)
+def change_test_dir(request, monkeypatch):
+    """No matter where pytest is ran, we set the working dir
+    to this testscript's parent directory"""
+
+    monkeypatch.chdir(request.fspath.dirname)
+
+
+def test_sim():
+    mpmModel = run_sim()
+
+    res = np.array([mp.getResultArray("displacement") for mp in mpmModel.materialPoints.values()])
+    gold = np.loadtxt("gold.csv")
+
+    print(res - gold)
+
+    assert np.isclose(res, gold).all()
+
+
+if __name__ == "__main__":
+    mpmModel = run_sim()
+
+    print("elapsed time: {:}".format(performancetiming.times["simulation"].time))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--create-gold", dest="create_gold", action="store_true", help="create the gold file.")
+    args = parser.parse_args()
+
+    if args.create_gold:
+        gold = np.array([mp.getResultArray("displacement") for mp in mpmModel.materialPoints.values()])
+        np.savetxt("gold.csv", gold)
---
+++ mpm/constraints/penaltyequalvalue.py
@@ -0,0 +1,133 @@
+from fe.config.phenomena import getFieldSize
+from fe.variables.scalarvariable import ScalarVariable
+from fe.timesteppers.timestep import TimeStep
+from mpm.models.mpmmodel import MPMModel
+from mpm.constraints.base.mpmconstraintbase import MPMConstraintBase
+from mpm.materialpoints.base.mp import MaterialPointBase
+import numpy as np
+
+
+class PenaltyEqualValue(MPMConstraintBase):
+    """
+    This is an implementation of an equal value constraint using a penalty formulation.
+
+    Parameters
+    ----------
+    name
+        The name of this constraint.
+    model
+        The full MPMModel instance.
+    constrainedMaterialPoints
+        The list of material points to be constrained.
+    field
+        The field this constraint is acting on.
+    prescribedComponent
+        The index of the constrained component.
+    penaltyParameter
+        The penalty parameter value.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        model: MPMModel,
+        constrainedMaterialPoints: list[MaterialPointBase],
+        field: str,
+        prescribedComponent: int,
+        penaltyParameter: float,
+    ):
+        self._name = name
+        self._model = model
+        self._constrainedMPs = constrainedMaterialPoints
+        self._field = field
+        self._prescribedComponent = prescribedComponent
+        self._fieldSize = getFieldSize(self._field, model.domainSize)
+        self._penaltyParameter = penaltyParameter
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def nodes(self) -> list:
+        return self._nodes
+
+    @property
+    def fieldsOnNodes(self) -> list:
+        return [
+            [
+                self._field,
+            ]
+        ] * len(self._nodes)
+
+    @property
+    def nDof(self) -> int:
+        return len(self._nodes) * self._fieldSize
+
+    @property
+    def scalarVariables(
+        self,
+    ) -> list:
+        return []
+
+    def getNumberOfAdditionalNeededScalarVariables(
+        self,
+    ) -> int:
+        return 0
+
+    def assignAdditionalScalarVariables(self, scalarVariables: list[ScalarVariable]):
+        pass
+
+    def initializeTimeStep(self, model, timeStep):
+        self._nodes = [n for mp in self._constrainedMPs for c in mp.assignedCells for n in c.nodes]
+
+    def applyConstraint(self, dU: np.ndarray, PExt: np.ndarray, V: np.ndarray, timeStep: TimeStep):
+        P_Ai = PExt.reshape((-1, self._fieldSize))
+        dU_Bj = dU.reshape((-1, self._fieldSize))
+
+        K_AiBj = V.reshape(P_Ai.shape + dU_Bj.shape)
+
+        i = self._prescribedComponent
+
+        # Part 1: compute the mean values over all material points
+
+        dMeanValue_dDU_Bj = np.zeros_like(dU_Bj)
+        currentNodeIdx = 0
+        meanValue = 0.0
+        for mp in self._constrainedMPs:
+            center = mp.getVertexCoordinates()[0]
+
+            for c in mp.assignedCells:
+                nNodesCell = c.nNodes
+                nodeIdcs = slice(currentNodeIdx, currentNodeIdx + nNodesCell)
+
+                N = c.getInterpolationVector(center)
+
+                meanValue += N @ dU_Bj[nodeIdcs, i]
+                dMeanValue_dDU_Bj[nodeIdcs, i] += N
+
+                currentNodeIdx += nNodesCell
+
+        meanValue /= len(self._constrainedMPs)
+        dMeanValue_dDU_Bj /= len(self._constrainedMPs)
+
+        # Part 2: compute the penalized difference between mean value and mp value:
+        currentNodeIdx = 0
+        for mp in self._constrainedMPs:
+            center = mp.getVertexCoordinates()[0]
+
+            for c in mp.assignedCells:
+                nNodesCell = c.nNodes
+
+                N = c.getInterpolationVector(center)
+
+                nodeIdcs = slice(currentNodeIdx, currentNodeIdx + nNodesCell)
+
+                mpValue = N @ dU_Bj[nodeIdcs, i]
+
+                P_Ai[nodeIdcs, i] += N * self._penaltyParameter * (mpValue - meanValue)
+
+                K_AiBj[nodeIdcs, i, nodeIdcs, i] += np.outer(N, N) * self._penaltyParameter
+                K_AiBj[nodeIdcs, i, :, i] += np.outer(N, dMeanValue_dDU_Bj[:, i]) * -1 * self._penaltyParameter
+
+                currentNodeIdx += nNodesCell
